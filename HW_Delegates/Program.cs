54718586e8cce61687f1f671943e6d4ef38090f0
kyzya30 <kyzya30@inbox.ru>
2015-08-23 10:12:30 +0300

    1

---
+++ HW_Delegates/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace HW_Delegates
+{
+    public delegate double ArifmDelagate(double a, double b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArifmDelagate add = (a, b) => a+b;
+            ArifmDelagate substract = (a, b) => a - b;
+            ArifmDelagate multiply = (a, b) => a*b;
+            ArifmDelagate divide = (a, b) =>
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Error. Divide by zero");
+                    return 0;
+                }
+                else
+                {
+                    return a/b;
+                }
+            };
+            Console.WriteLine("Введите первое число:");
+            double firstNum = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double secondNum = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите тип арифметической операции (+,-,*,/)");
+            char operation = char.Parse(Console.ReadLine());
+            switch (operation)
+            {
+                case '+':
+                    {
+                        Console.WriteLine("Result:{0}", add(firstNum,secondNum));
+                    }
+                    break;
+                case '-':
+                    {
+                        Console.WriteLine("Result:{0}", substract(firstNum, secondNum));
+                    }
+                    break;
+                case '*':
+                    {
+                        Console.WriteLine("Result:{0}", multiply(firstNum, secondNum));
+                    }
+                    break;
+                case '/':
+                    {
+                        Console.WriteLine("Result:{0}", divide(firstNum, secondNum));
+                    }
+                    break;
+                default:
+                {
+                    Console.WriteLine("Unknown type of operation");
+                }
+                    break;
+
+
+            }
+        }
+
+        
+            
+        }
+    }
+
---
+++ HW_delegates_Task21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace HW_delegates_Task21
+{
+    class Program
+    {
+        public delegate double ArrDelegate();
+
+        public delegate double Delegate(ArrDelegate[] delegates);
+        static void Main(string[] args)
+        {
+            Random randVal = new Random();
+            ArrDelegate[]  delegates = new ArrDelegate[5];
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                delegates[i] = () => randVal.Next(10);
+            }
+            Delegate arrDel = myDelegates =>     
+            {
+                double sum = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    sum += delegates[i]();
+                }               
+                Console.WriteLine("sum is -> {0}",sum);
+                return sum/5;
+            };
+            Console.WriteLine("Среднее арифметическое : {0}", arrDel(delegates));
+            Console.WriteLine("Значения переменных в делегатах: ");
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                Console.WriteLine("{0}", delegates[i]());
+            }
+        }
+    }
+}
